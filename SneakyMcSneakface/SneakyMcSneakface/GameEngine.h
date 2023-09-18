60cb1fb694a1d13b9cb6846e8d1bcbbcee3e9ed9
jacquelynneheiman1209 <jacquelynneheiman1209@gmail.com>
2023-09-18 12:14:52 -0700

    Implemented the Input, Update and Draw interfaces in the game engine

--- SneakyMcSneakface/SneakyMcSneakface/GameEngine.h
+++ SneakyMcSneakface/SneakyMcSneakface/GameEngine.h
@@ -1,18 +1,35 @@
 #pragma once
 #include "IInitialize.h"
+#include "IInput.h"
+#include "IUpdate.h"
+#include "IDraw.h"
 
 #ifndef GAME_ENGINE_H
 #define GAME_ENGINE_H
 
-class GameEngine : IInitialize
+class GameEngine : IInitialize, IInput, IUpdate, IDraw
 {
 public:
 	GameEngine();
 
-	// initialize the game engine
 	bool IInitialize::initialize()
 	{
 		return true;
+	}
+
+	void IInput::handleInput(sf::RenderWindow* window, sf::Event* event)
+	{
+
+	}
+
+	void IUpdate::update(float deltaTime)
+	{
+
+	}
+
+	void IDraw::draw(sf::RenderWindow* window)
+	{
+
 	}
 };
 
